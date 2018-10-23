cbf2cf6a20d8208f1578b73fa48db74e2d61d957
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2018-10-22 21:19:40 -0500

    Updating processor yaml

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@
 singularity run \
 --bind INPUTS:/INPUTS \
 --bind OUTPUTS:/OUTPUTS \
-MP2RAGE_v1.0.1.simg \
+baxpr-mp2rage-master-v1.0.1.simg \
 --mp2rage_dir /INPUTS/NIFTI \
 --project PROJNAME \
 --subject SUBJNAME \
